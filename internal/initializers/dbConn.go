31e32ad44995e31821571d51e95b7e7e4e4fb0b9
whotterre <ijeddy5.0@gmail.com>
2026-07-12 06:28:17 +0100

    chore: adapt config loader to match env files in Makefile

--- internal/initializers/dbConn.go
+++ internal/initializers/dbConn.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"os"
@@ -9,13 +10,18 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func ConnectToDB(ctx context.Context) (*pgxpool.Pool, error){
-	DB_URL := os.Getenv("DB_URL")
+func ConnectToDB(ctx context.Context) (*pgxpool.Pool, error) {
+	DB_USER := os.Getenv("DATABASE_USER")
+	DB_PASS := os.Getenv("DATABASE_PASS")
+	DB_HOST := os.Getenv("DATABASE_HOST")
+	DB_NAME := os.Getenv("DATABASE_NAME")
+	DB_PORT := os.Getenv("DATABASE_PORT")
+	DB_URL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
+	log.Println(DB_URL)
 	pool, err := pgxpool.New(ctx, DB_URL)
 	if err != nil {
 		log.Println("Failed to log in to the db", err.Error())
 		return nil, err
 	}
-	log.Println("Connected to the db")
-	return pool, nil 
+	return pool, nil
 }
